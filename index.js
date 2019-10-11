de091786a277a019b347edb5d9757b3c010007b7
Olivier Verhaegen <olivierverhaegen1@hotmail.com>
2019-10-11 18:43:35 +0200

    Added more example code

--- index.js
+++ index.js
@@ -3,18 +3,28 @@ const https = require('https');
 const express = require('express');
 const morgan = require('morgan');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 const logger = require('./logger');
 
 const app = express();
 app.use(express.static('./public'));
 app.use(morgan('short'));
+app.use(bodyParser.urlencoded({extended: false}));
 
+//----------------------------------------------------
+//                     SQL Connection
+//----------------------------------------------------
 const mysqlConnectionConfig= {
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'HAP'
+};
+
+function getSQLConnection() {
+    return mysql.createConnection(mysqlConnectionConfig);
 }
+//----------------------------------------------------
 
 app.get('/', (req, res) => {
     logger.warn('WARN: Someone is trying to access the root route.');
@@ -24,7 +34,7 @@ app.get('/', (req, res) => {
 app.get('/products', (req, res) => {
     logger.log('Getting products from database...');
     
-    const connection = mysql.createConnection(mysqlConnectionConfig);
+    const connection = getSQLConnection();
 
     const queryString = 'SELECT * FROM products';
     connection.query(queryString, (err, rows, fields) => {
@@ -41,7 +51,7 @@ app.get('/products', (req, res) => {
 app.get('/products/:id', (req, res) => {
     logger.log(`Getting product ${req.params.id} from database...`);
 
-    const connection = mysql.createConnection(mysqlConnectionConfig);
+    const connection = getSQLConnection();
 
     const queryString = 'SELECT * FROM products WHERE id = ?';
     connection.query(queryString, [req.params.id], (err, rows, fields) => {
@@ -53,6 +63,30 @@ app.get('/products/:id', (req, res) => {
 
         res.json(rows);
     });
+});
+
+app.post('/products/create', (req, res) => {
+    logger.log('Creating user' + req.body.name);
+
+    const queryString = 'INSERT INTO products (name, available, price) VALUES (?, ?, ?)';
+    getSQLConnection().query(
+        queryString,
+        [   req.body.name,
+            req.body.available,
+            req.body.price
+        ], (err, result, fields) => {
+            if (err) {
+                logger.error('Failed to insert new user: ' + err);
+                res.sendStatus(500);
+                return;
+            }
+
+            //TODO: Insert product into database. 
+
+            logger.log('Inserted new product with id: ' + result.insertId)
+        });
+    
+    res.end();
 });
 
 http.createServer(app).listen(9000, () => {
