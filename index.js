dbd237c5b3db9446b59d60296d885a60674751c5
Olivier Verhaegen <olivierverhaegen1@hotmail.com>
2019-10-11 19:06:41 +0200

    Code refacture

--- index.js
+++ index.js
@@ -6,88 +6,22 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const logger = require('./logger');
 
+const productsRoute = require('./routes/products');
+
 const app = express();
+
+app.use(productsRoute);
 app.use(express.static('./public'));
 app.use(morgan('short'));
 app.use(bodyParser.urlencoded({extended: false}));
 
-//----------------------------------------------------
-//                     SQL Connection
-//----------------------------------------------------
-const mysqlConnectionConfig= {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'HAP'
-};
-
-function getSQLConnection() {
-    return mysql.createConnection(mysqlConnectionConfig);
-}
-//----------------------------------------------------
-
+// Respond to root.
 app.get('/', (req, res) => {
     logger.warn('WARN: Someone is trying to access the root route.');
     res.status(404).send();
 });
 
-app.get('/products', (req, res) => {
-    logger.log('Getting products from database...');
-    
-    const connection = getSQLConnection();
 
-    const queryString = 'SELECT * FROM products';
-    connection.query(queryString, (err, rows, fields) => {
-        if (err) {
-            logger.error('ERROR: Failed to get products from database.');
-            res.sendStatus(500);
-            return;
-        }
-
-        res.json(rows);
-    });
-});
-
-app.get('/products/:id', (req, res) => {
-    logger.log(`Getting product ${req.params.id} from database...`);
-
-    const connection = getSQLConnection();
-
-    const queryString = 'SELECT * FROM products WHERE id = ?';
-    connection.query(queryString, [req.params.id], (err, rows, fields) => {
-        if (err) {
-            logger.error('ERROR: Failed to get products from database.');
-            res.sendStatus(500);
-            return;
-        }
-
-        res.json(rows);
-    });
-});
-
-app.post('/products/create', (req, res) => {
-    logger.log('Creating user' + req.body.name);
-
-    const queryString = 'INSERT INTO products (name, available, price) VALUES (?, ?, ?)';
-    getSQLConnection().query(
-        queryString,
-        [   req.body.name,
-            req.body.available,
-            req.body.price
-        ], (err, result, fields) => {
-            if (err) {
-                logger.error('Failed to insert new user: ' + err);
-                res.sendStatus(500);
-                return;
-            }
-
-            //TODO: Insert product into database. 
-
-            logger.log('Inserted new product with id: ' + result.insertId)
-        });
-    
-    res.end();
-});
 
 http.createServer(app).listen(9000, () => {
     logger.log("Server is up an listening on http://localhost:9000");
---
+++ routes/products.js
@@ -0,0 +1,74 @@
+const logger = require('../logger');
+const express = require('express');
+const mysql = require('mysql');
+const router = express.Router();
+
+//----------------------------------------------------
+//                     SQL Connection
+//----------------------------------------------------
+function getSQLConnection() {
+    return mysql.createPool({
+        connectionLimit: 10,
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'HAP'
+    });
+}
+//----------------------------------------------------
+
+router.get('/products', (req, res) => {
+    logger.log('Getting products from database...');
+
+    const queryString = 'SELECT * FROM products';
+    getSQLConnection().query(queryString, (err, rows, fields) => {
+        if (err) {
+            logger.error('ERROR: Failed to get products from database.');
+            res.sendStatus(500);
+            return;
+        }
+
+        res.json(rows);
+    });
+});
+
+router.get('/products/:id', (req, res) => {
+    logger.log(`Getting product ${req.params.id} from database...`);
+
+    const queryString = 'SELECT * FROM products WHERE id = ?';
+    getSQLConnection().query(queryString, [req.params.id], (err, rows, fields) => {
+        if (err) {
+            logger.error('ERROR: Failed to get products from database.');
+            res.sendStatus(500);
+            return;
+        }
+
+        res.json(rows);
+    });
+});
+
+router.post('/products/create', (req, res) => {
+    logger.log('Creating user' + req.body.name);
+
+    const queryString = 'INSERT INTO products (name, available, price) VALUES (?, ?, ?)';
+    getSQLConnection().query(
+        queryString,
+        [   req.body.name,
+            req.body.available,
+            req.body.price
+        ], (err, result, fields) => {
+            if (err) {
+                logger.error('Failed to insert new user: ' + err);
+                res.sendStatus(500);
+                return;
+            }
+
+            //TODO: Insert product into database. 
+
+            logger.log('Inserted new product with id: ' + result.insertId)
+        });
+    
+    res.end();
+});
+
+module.exports = router;
