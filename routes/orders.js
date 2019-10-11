ecfd41d72de144c304c29b7ffdbaa35e7d3f0b14
Olivier Verhaegen <olivierverhaegen1@hotmail.com>
2019-10-11 21:22:50 +0200

    Refactored mySQL connection method

--- routes/orders.js
+++ routes/orders.js
@@ -1,27 +1,14 @@
 const logger = require('../logger');
 const express = require('express');
-const mysql = require('mysql');
 const router = express.Router();
 
-//----------------------------------------------------
-//                     SQL Connection
-//----------------------------------------------------
-function getSQLConnection() {
-    return mysql.createPool({
-        connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'HAP'
-    });
-}
-//----------------------------------------------------
+const SQLConnection = require('../database');
 
 router.get('/', (req, res) => {
     logger.log('Getting orders from database...');
 
     const queryString = 'SELECT * FROM orders';
-    getSQLConnection().query(queryString, (err, rows, fields) => {
+    SQLConnection().query(queryString, (err, rows, fields) => {
         if (err) {
             logger.error('Failed to get orders from database.');
             res.sendStatus(500);
--- routes/products.js
+++ routes/products.js
@@ -1,27 +1,14 @@
 const logger = require('../logger');
 const express = require('express');
-const mysql = require('mysql');
 const router = express.Router();
 
-//----------------------------------------------------
-//                     SQL Connection
-//----------------------------------------------------
-function getSQLConnection() {
-    return mysql.createPool({
-        connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'HAP'
-    });
-}
-//----------------------------------------------------
+const SQLConnection = require('../database');
 
 router.get('/', (req, res) => {
     logger.log('Getting products from database...');
 
     const queryString = 'SELECT * FROM products';
-    getSQLConnection().query(queryString, (err, rows, fields) => {
+    SQLConnection().query(queryString, (err, rows, fields) => {
         if (err) {
             logger.error('Failed to get products from database.');
             res.sendStatus(500);
@@ -36,7 +23,7 @@ router.get('/:id', (req, res) => {
     logger.log(`Getting product ${req.params.id} from database...`);
 
     const queryString = 'SELECT * FROM products WHERE id = ?';
-    getSQLConnection().query(queryString, [req.params.id], (err, rows, fields) => {
+    SQLConnection().query(queryString, [req.params.id], (err, rows, fields) => {
         if (err) {
             logger.error('Failed to get products from database.');
             res.sendStatus(500);
@@ -51,7 +38,7 @@ router.post('/create', (req, res) => {
     logger.log('Creating user' + req.body.name);
 
     const queryString = 'INSERT INTO products (name, available, price) VALUES (?, ?, ?)';
-    getSQLConnection().query(
+    SQLConnection().query(
         queryString,
         [   req.body.name,
             req.body.available,
