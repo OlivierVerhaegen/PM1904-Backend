3ff5e0666096790e79cec2dfe8ef8d605a0e84ec
Olivier Verhaegen <olivierverhaegen1@hotmail.com>
2019-10-23 17:12:40 +0200

    Added validation for student number

--- routes/user.js
+++ routes/user.js
@@ -4,3 +4,14 @@ const router = express.Router();
 
 const SQLConnection = require('../database');
 
+function validateStudentNumber(studentNumber) {
+    const lowerCase = studentNumber.toLowerCase();
+    const startsWithS = lowerCase.startsWith('s');
+    const correctLength = lowerCase.length == 9;
+
+    if (startsWithS && correctLength) {
+        return true;
+    } else {
+        return false;
+    }
+}
