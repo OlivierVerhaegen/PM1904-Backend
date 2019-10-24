818ca2e60592d323007a76cd1eb665c99378ba65
Olivier Verhaegen <olivierverhaegen1@hotmail.com>
2019-10-24 11:54:24 +0200

    Credentials fix

--- database.js
+++ database.js
@@ -1,11 +1,6 @@
 const mysql = require('mysql');
+const credentials = require('./credentials');
 
 module.exports = () => {
-    return mysql.createPool({
-        connectionLimit: 10,
-        host: 'vserver122.axc.eu',
-        user: 'philivg122_hap',
-        password: 'zenohattas123',
-        database: 'philivg122_hap'
-    });
+    return mysql.createPool(credentials);
 }
