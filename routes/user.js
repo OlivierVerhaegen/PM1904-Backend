09a51eac138f6592d80da6d0874207f4e2585bce
ben <s108652@ap.be>
2019-10-24 10:50:09 +0200

    user toevoegen/updaten/deleten

--- routes/user.js
+++ routes/user.js
@@ -55,3 +55,87 @@ router.get('/home', function(req, res) {
 	}
 	res.end();
 });
+
+
+
+//--------------------------------------------------------------------------------------
+//                                         POST REQUESTS
+//-------------------------------------------------------------
+
+
+router.post('/create', (req, res) => {
+    logger.info('Creating user ' + req.body.userName);
+
+    const queryString = 'INSERT INTO user (name, id, password) VALUES (?, ?, ?)';
+    SQLConnection().query(
+        queryString,
+        [   req.body.userName,
+            req.body.userId,
+            req.body.password
+            
+        ], (err, result, fields) => {
+            if (err) {
+                logger.error('Failed to insert new user: ' + err);
+                res.status(500).redirect('/?status=error');
+                return;
+            } 
+
+            logger.success('Inserted new user with id: ' + result.insertId)
+            res.status(201).redirect('/?status=success');
+        }
+    );
+
+});
+//--------------------------------------------------------------------------------------
+//                                         UPDATE REQUESTS
+//--------------------------------------------------------------------------------------
+router.patch('/:id', (req, res) => {
+    logger.log('Updating user with id: ' + req.params.id);
+
+    const queryString = 'UPDATE user SET orderNumber = ?, products = ?, subtotal = ? WHERE id = ?';
+    SQLConnection().query(
+      queryString,
+      [
+          req.body.userId,
+          req.body.userName,
+          req.body.password,
+          
+      ],
+      (err, result, fields) => {
+          if (err) {
+              logger.error('Failed to update user with id: ' + req.params.id)
+              res.status(500).redirect('/?status=error');
+              return;
+          }
+
+          logger.success('Updated user with id: ' + req.params.id);
+          res.status(200).redirect('/?status=success');
+      }
+    );
+});
+
+//--------------------------------------------------------------------------------------
+//                                         DELETE REQUESTS
+//--------------------------------------------------------------------------------------
+router.delete('/:id', (req, res) => {
+    logger.log('Deleting user with id: ' + req.params.id);
+
+    const queryString = 'DELETE FROM user WHERE id = ?';
+    SQLConnection().query(queryString, [req.params.id], (err, reslut, fields) => {
+        if (err) {
+            logger.error('Failed to delete user with id: ' + req.params.id);
+            res.status(500).redirect('/?status=error');
+            return;
+        }
+
+        logger.success('Deleted user with id: ' + req.params.id);
+        res.status(200).redirect('/?status=success');
+    });
+});
+
+
+
+
+
+
+module.exports = router;
